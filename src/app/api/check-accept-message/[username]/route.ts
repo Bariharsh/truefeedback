41dced02867abadfa9be6b094562adfbb66d7203
Bariharsh <144584767+Bariharsh@users.noreply.github.com>
2025-07-24 16:13:20 +0530

    try4

--- src/app/api/check-accept-message/[username]/route.ts
+++ src/app/api/check-accept-message/[username]/route.ts
@@ -2,13 +2,12 @@ import { NextRequest, NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { username: string } }
-) {
+export async function GET(req: NextRequest, context: { params: Record<string, string> }) {
+  const { username } = context.params;
+
   await dbConnect();
 
-  const user = await UserModel.findOne({ username: params.username });
+  const user = await UserModel.findOne({ username });
 
   if (!user) {
     return NextResponse.json({ message: "User not found" }, { status: 404 });
