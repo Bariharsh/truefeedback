4758892661957667b61258ae90382057243798c1
Bariharsh <144584767+Bariharsh@users.noreply.github.com>
2025-07-24 15:55:30 +0530

    Refactor GET function: improve parameter handling and enhance code readability

--- src/app/api/check-accept-message/[username]/route.ts
+++ src/app/api/check-accept-message/[username]/route.ts
@@ -2,13 +2,18 @@ import { NextRequest,NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: { username: string } }
-) {
+interface Context {
+  params: {
+    username: string;
+  };
+}
+
+
+
+export async function GET(req: NextRequest, context: Context) {
   await dbConnect();
 
-  const user = await UserModel.findOne({ username: params.username });
+  const user = await UserModel.findOne({ username: context.params.username });
 
   if (!user) {
     return NextResponse.json({ message: "User not found" }, { status: 404 });
