0cb192521d77e51eb08ddd049ceab929de41098b
Bariharsh <144584767+Bariharsh@users.noreply.github.com>
2025-07-25 19:17:24 +0530

    fix: update environment variable configuration to use NEXT_PUBLIC_BASE_URL

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-//   env: {
-//     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-//   },
+   env: {
+     NEXT_PUBLIC_BASE_URL: process.env.NEXTAUTH_URL,
+   },
 };
 
 module.exports = nextConfig;
