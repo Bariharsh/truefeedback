9e78feb66926bf475c9a15982224958e0c7181a9
Bariharsh <144584767+Bariharsh@users.noreply.github.com>
2025-07-25 19:25:30 +0530

    try4

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   env: {
-     NEXT_PUBLIC_BASE_URL: process.env.NEXTAUTH_URL,
-   },
+
 };
 
 module.exports = nextConfig;
